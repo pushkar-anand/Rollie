7a2e6634cb8a50dc0adfdf63e4026908342b2167
Pushkar Anand <anandpushkar088@gmail.com>
2020-02-09 17:38:02 +0530

    Finished options.

---
+++ src/js/options.js
@@ -0,0 +1,26 @@
+const DEFAULT_TIME = 5;
+const TAB_TIME_KEY = "TAB_TIME";
+const storage = chrome.storage.local;
+
+$(document).ready(() => {
+	storage.get(null, setSavedValues);
+	M.updateTextFields();
+});
+
+$("#save-options").click( ()=> {
+	const values = {};
+	values[TAB_TIME_KEY] = $("#minutes").val();
+	storage.set(values, () => {
+		M.toast({html: 'Options Saved!!'});
+	})
+});
+
+
+const setSavedValues = (result) => {
+	const inputMinutes = $("#minutes");
+	if ( result[TAB_TIME_KEY] === undefined) {
+		inputMinutes.val(DEFAULT_TIME);
+	} else {
+		inputMinutes.val(result[TAB_TIME_KEY]);
+	}
+};
